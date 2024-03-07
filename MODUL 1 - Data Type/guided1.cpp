97b753900495cd55ee8bbb877259be728137ee0a
agungibr <agungibr@outlook.co.id>
2024-03-07 14:01:28 +0700

    primitive data types

---
+++ MODUL 1 - Data Type/guided1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+// Main program
+int main() 
+{
+	char op;
+	float num1, num2;
+
+	//It allows user to enter operator i.e. +, -, *, /
+	cin >> op;
+
+	//It allows user to enter the operations
+	cin >> num1 >> num2;
+
+	//Switch statement begins
+	switch (op)
+	{
+	//If user enter +
+	case '+':
+		cout << num1 + num2;
+		break;
+
+	//If user enter -
+	case '-':
+		cout << num1 - num2;
+		break;
+
+	//If user enter *
+	case '*':
+		cout << num1 * num2;
+		break;
+
+	//If user enter /
+	case '/':
+		cout << num1 / num2;
+		break;
+
+	//If the operatior is other that +, -, * or /,
+	//error message will display
+	default:
+		cout << "Error! operator is not correct";
+	}//switch statement ends
+	return 0;
+}
