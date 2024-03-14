d8d89f6748a6890434bed1fc61d5260e47f3792e
agungibr <agungibr@outlook.co.id>
2024-03-14 21:54:03 +0700

    fix done

--- MODUL 1 - Data Type/unguided1.cpp
+++ MODUL 1 - Data Type/unguided1.cpp
@@ -5,7 +5,7 @@ char menu; //mendeklarasikan variabel menu dengan type data character
 float fahrenheit,kelvin,one; //mendeklarasikan variabel dengan type data float
 
 void choice() { //variable choice untuk memilih menu yang ingin digunakan
-	cout <<"\n=====Temperature Converter Celsius====="<< endl;
+	cout <<"\n=====Temperature Converter Celsius====="<< endl; //menu
 	cout << "A. Celsius to Fahrenheit" << endl;
 	cout << "B. Celsius to Kelvin" << endl;
 	cout << "0. Exit" << endl;
@@ -14,7 +14,7 @@ void choice() { //variable choice untuk memilih menu yang ingin digunakan
 }
 
 float fahr() { //variable untuk menghitung celcius ke fahrenheit dengan type data float
-	cout << "Input temperature in Celsius: ";
+	cout << "Input temperature in Celsius: "; //menginputkan suhu dalam celsius
 	cin >> one;
 
 	fahrenheit = (1.8 * one) + 32.0; //rumus menghitung celsius ke fahrenheit
@@ -24,12 +24,12 @@ float fahr() { //variable untuk menghitung celcius ke fahrenheit dengan type dat
 }
 
 float kelv() { //variable untuk menghitung celcius ke kelvin dengan type data float
-	cout << "Input temperature in Celsius: ";
+	cout << "Input temperature in Celsius: "; //menginputkan suhu dalam celcius
 	cin >> one;
 
 	kelvin = 273.15 + one; //rumus menghitung celsius ke kelvin
 
-	cout << "Temperature in degree Kelvin: " << kelvin << " K" << endl;
+	cout << "Temperature in degree Kelvin: " << kelvin << " K" << endl; //menampilkan hasil
 	return kelvin = 273.15 + one;
 }
 
@@ -39,22 +39,22 @@ int main() { //menu utama
         switch (menu) {
             case 'A': //jika memilih A/a maka akan menghitung fahrenheit
             case 'a':
-                fahr();
+                fahr(); //menampilkan variable celcius ke fahrenheit
                 break;
 
             case 'B': //jika memilih B/b maka akan menghitung kelvin
             case 'b':
-                kelv();
+                kelv(); //menampilkan variable celsius ke kelvin
                 break;
                 
             case '0': //jika ingin keluar dari looping maka bisa menginputkan angka 0
                 cout << "Thank you for using this program. Goodbye!" << endl;
                 break;
 
-            default: //jika menginputkan angka selain diatas maka akan muncul message error seperti dibawah ini
-                cout << "Error, operator is not correct";
-                return 0;
+            default: //error message jika menginputkan selain huruf a, b, dan angka 0
+    			cout << "Invalid input. Please enter either 'A', 'B', or '0'." << endl;
+    			break;
         }
-    } while (menu != '0'); //jika user menginputkan angka 0 maka program otomatis berhenti
+    } while (menu != '0'); //jika user menginputkan angka selain 0 maka program otomatis akan mengulang hingga user menginputkan angka 0 untuk mengakhiri loop
     return 0;
 }
