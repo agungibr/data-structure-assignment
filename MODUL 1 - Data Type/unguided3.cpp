40e4c29b14e003608434b464f835dcaf4e2d3633
agungibr <agungibr@outlook.co.id>
2024-03-15 08:11:57 +0700

    add comment

--- MODUL 1 - Data Type/unguided3.cpp
+++ MODUL 1 - Data Type/unguided3.cpp
@@ -1,16 +1,18 @@
-#include <iostream>
-#include <map>
-using namespace std;
+#include <iostream> //input preprocessor directive
+#include <map> //input map
+using namespace std; //supaya tidak perlu menggunakan std:: disetiap input object or function
 
-int main() {
-	map <int, string> character;
+int main() { //main program 
+	map <int, string> character; //membuat map dengan key int dan bertipe data string
 
+	//menginputkan karakter
 	character[0] = "Spongebob Squarepants";
 	character[1] = "Patrick Star";
 	character[2] = "Sandy Cheeks";
 	character[3] = "Squidward Tentacles";
 	character[4] = "Mr. Krabs";
 
+	//menampilkan karakter yang sudah dibuat
 	cout << "==========CHARACTER LIST==========" << endl;
 	cout << "Karakter pertama : " << character[0] << endl;
 	cout << "Karakter kedua : " << character[1] << endl;
